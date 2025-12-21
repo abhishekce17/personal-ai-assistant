9fb042d0c150e75f8f47474dc6d9795fa089e231
abhishekce17 <abhisheklprajapati18@gmail.com>
2025-12-21 11:52:17 +0530

    refactor: Remove unused helper scripts and main application code

--- helper_scripts.py
+++
@@ -1,8 +0,0 @@
-# I'll use this file to create all db table and insert values of tools, features, model and all other thing which should be in the db to be used by the user
-
-#Tables
-# Tools 
-# Features
-# Plan 
-# PlanTool 
-# PlanModel
